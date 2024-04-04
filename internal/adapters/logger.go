99e5f96439f70d9e3d2efc17f88551acdd90c228
Aleksandr Arofikin <sashaaro@gmail.com>
2024-04-04 17:36:29 +0300

    increment #6, add logging middleware

---
+++ internal/adapters/logger.go
@@ -0,0 +1,17 @@
+package adapters
+
+import "go.uber.org/zap"
+
+var Logger zap.SugaredLogger
+
+func init() {
+	l, err := zap.NewDevelopment()
+	if err != nil {
+		// вызываем панику, если ошибка
+		panic(err)
+	}
+	//nolint:errcheck
+	defer l.Sync()
+
+	Logger = *l.Sugar()
+}
